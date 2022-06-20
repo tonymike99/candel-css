f0dc70981b010bdb21a5fe762959608ba7804326
Tony <tonymike99@gmail.com>
2022-06-20 12:43:01 +0530

    modified scripts for dark-theme and loader

---
+++ assets/js/script-docs.js
@@ -0,0 +1,59 @@
+// Lazy load for img and iframe
+
+Array.from(document.getElementsByTagName("img")).map((imgEle) =>
+  imgEle.setAttribute("loading", "lazy")
+);
+
+Array.from(document.getElementsByTagName("iframe")).map((iframeEle) =>
+  iframeEle.setAttribute("loading", "lazy")
+);
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+// <!-- Modal -->
+
+const modal = document.getElementsByClassName("modal")[0];
+const modalOpen = document.getElementById("modal-open");
+const modalClose = document.getElementById("modal-close");
+
+modalOpen.addEventListener("click", () => {
+  modal.classList.add("modal-visible");
+});
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("modal-visible");
+});
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+// <!-- Toast -->
+
+// Primary toast
+
+const toastPrimaryBtn = document.getElementById("toast-primary");
+const toast1 = document.getElementById("toast1");
+
+toastPrimaryBtn.addEventListener("click", () => {
+  toast1.classList.add("toast-visible");
+  setTimeout(() => toast1.classList.remove("toast-visible"), 1500);
+});
+
+toast1.addEventListener("click", () => {
+  toast1.classList.remove("toast-visible");
+});
+
+// Danger toast
+
+const toastDangerBtn = document.getElementById("toast-danger");
+const toast2 = document.getElementById("toast2");
+
+toastDangerBtn.addEventListener("click", () => {
+  toast2.classList.add("toast-visible");
+  setTimeout(() => toast2.classList.remove("toast-visible"), 1500);
+});
+
+toast2.addEventListener("click", () => {
+  toast2.classList.remove("toast-visible");
+});
+
+/* ---------------------------------------------------------------------------------------------------- */
